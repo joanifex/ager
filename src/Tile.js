2e72f5bcedb379be7f8c8dd1ae95530ac7d73b8f
Ian Wright <ian@authentise.com>
2018-02-06 18:33:42 -0700

    Refactor tiles data

--- src/Tile.js
+++ src/Tile.js
@@ -1,30 +1,20 @@
 import React from 'react';
-
-const tiles = {
-  grass: {
-    color: '#5A783A',
-  },
-  forest: {
-    color: '#2A381B', 
-  },
-  mountain: {
-    color: '#B2782F',
-  },
-  lake: {
-    color: '#5399FF',
-  },
-};
+import PropTypes from 'prop-types';
+import TILES from './constants/tiles';
 
 const Tile = ({tile}) => (
   <svg width="100%" height="100%">
     <rect
       width="100%"
       height="100%"
-      fill={tiles[tile] ? tiles[tile].color : 'black'}
+      fill={TILES[tile] ? TILES[tile].color : 'black'}
       stroke="black"
     />
   </svg>
 );
 
+Tile.propTypes = {
+  tile: PropTypes.oneOf(['grass', 'forest', 'lake', 'mountain'])
+};
 
 export default Tile;

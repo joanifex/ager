2098a36bd5801e1dcfa54674cb5e5ebf7d5f6bc7
Ian Wright <ian@authentise.com>
2018-02-07 16:02:54 -0700

    Add foodProduction to Stats

--- src/Stats.js
+++ src/Stats.js
@@ -1,31 +1,47 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import tileData from './data/tiles';
 
-export const Stats = ({ availablePopulations, food }) => (
+export const Stats = ({ availablePopulations, foodProduction, foodStored }) => (
   <header>
     <ul>
       <li>Available Population: {availablePopulations}</li>
     </ul>
     <ul>
-      <li>Food: {food}</li>
+      <li>Food Stored: {foodStored}</li>
+    </ul>
+    <ul>
+      <li>Food Production: {foodProduction}</li>
     </ul>
   </header>
 );
 
 Stats.defaultProps = {
   availablePopulations: 0,
-  food: 0
+  foodProduction: 0,
+  foodStored: 0
 };
 
 Stats.propTypes = {
   availablePopulations: PropTypes.number.isRequired,
-  food: PropTypes.number.isRequired
+  foodProduction: PropTypes.number.isRequired,
+  foodStored: PropTypes.number.isRequired
 };
 
-export default connect(({ populations }) => ({
+export default connect(({ populations, tiles }) => ({
   availablePopulations: populations.reduce(
     (sum, population) => (population.populating === null ? sum + 1 : sum),
     0
-  )
+  ),
+  foodProduction: populations.reduce(
+    (sum, population) =>
+      population.populating
+        ? sum +
+          tileData[tiles.find(tile => tile.id === population.populating).type]
+            .food
+        : sum,
+    0
+  ),
+  foodStored: 0
 }))(Stats);

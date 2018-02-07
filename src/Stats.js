c290c3b39463bee87d21f54e60184284cd145dc1
Ian Wright <ian@authentise.com>
2018-02-07 15:45:32 -0700

    Add available population to stats

--- src/Stats.js
+++ src/Stats.js
@@ -2,8 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-export const Stats = ({ food }) => (
+export const Stats = ({ availablePopulations, food }) => (
   <header>
+    <ul>
+      <li>Available Population: {availablePopulations}</li>
+    </ul>
     <ul>
       <li>Food: {food}</li>
     </ul>
@@ -11,13 +14,18 @@ export const Stats = ({ food }) => (
 );
 
 Stats.defaultProps = {
+  availablePopulations: 0,
   food: 0
 };
 
 Stats.propTypes = {
+  availablePopulations: PropTypes.number.isRequired,
   food: PropTypes.number.isRequired
 };
 
-export default connect(({ food }) => ({
-  food
+export default connect(({ populations }) => ({
+  availablePopulations: populations.reduce(
+    (sum, population) => (population.populating === null ? sum + 1 : sum),
+    0
+  )
 }))(Stats);

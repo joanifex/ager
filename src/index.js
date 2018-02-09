dff45211c95e58635fef3542bed3982874e199c8
Ian Wright <ian@authentise.com>
2018-02-09 15:07:20 -0700

    Add material-ui and roboto

--- src/index.js
+++ src/index.js
@@ -4,16 +4,17 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
 import App from './components/App';
+import 'typeface-roboto';
 
 ReactDOM.render(
   <Provider
     store={createStore(
       reducers,
       window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
+        window.__REDUX_DEVTOOLS_EXTENSION__(),
     )}
   >
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );

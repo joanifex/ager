f4bbe5517bc6bf6553714a507616d233f5e566f2
Ian Wright <ian@authentise.com>
2018-02-10 16:21:38 -0700

    Add actionCreator tests

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,19 +1,20 @@
-export const endTurn = endTurnData => ({
-  type: 'END_TURN',
-  ...endTurnData,
-});
-
 export const createPopulation = () => ({
   type: 'CREATE_POPULATION',
+});
+
+export const endTurn = ({ foodProduction, populationLoss }) => ({
+  type: 'END_TURN',
+  foodProduction,
+  populationLoss,
+});
+
+export const populationDepopulates = ({ populationId }) => ({
+  type: 'POPULATION_DEPOPULATES',
+  populationId,
 });
 
 export const populationPopulatesTile = ({ populationId, tileId }) => ({
   type: 'POPULATION_POPULATES_TILE',
   populationId,
   tileId,
-});
-
-export const populationDepopulates = ({ populationId }) => ({
-  type: 'POPULATION_DEPOPULATES',
-  populationId,
 });
---
+++ src/actions/index.test.js
@@ -0,0 +1,54 @@
+import {
+  createPopulation,
+  endTurn,
+  populationDepopulates,
+  populationPopulatesTile,
+} from './index';
+
+describe('endTurn', () => {
+  it('should create an action to end the turn', () => {
+    const endTurnData = {
+      foodProduction: 0,
+      populationLoss: false,
+    };
+    const expectedAction = {
+      type: 'END_TURN',
+      ...endTurnData,
+    };
+    expect(endTurn(endTurnData)).toEqual(expectedAction);
+  });
+});
+
+describe('createPopulation', () => {
+  it('should create an action to create a population', () => {
+    const expectedAction = {
+      type: 'CREATE_POPULATION',
+    };
+    expect(createPopulation()).toEqual(expectedAction);
+  });
+});
+
+describe('populationDepopulates', () => {
+  it('should create an action for a population to depopulate ', () => {
+    const populationId = 0;
+    const expectedAction = {
+      type: 'POPULATION_DEPOPULATES',
+      populationId,
+    };
+    expect(populationDepopulates({ populationId })).toEqual(expectedAction);
+  });
+});
+
+describe('populationPopulatesTile', () => {
+  it('should create an action for a population to populate a tile ', () => {
+    const populateData = {
+      populationId: 0,
+      tileId: 0,
+    };
+    const expectedAction = {
+      type: 'POPULATION_POPULATES_TILE',
+      ...populateData,
+    };
+    expect(populationPopulatesTile(populateData)).toEqual(expectedAction);
+  });
+});

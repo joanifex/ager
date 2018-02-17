e58d9ca97dba3c47083366f61585c96d550fd820
Ian Wright <ian@authentise.com>
2018-02-16 18:37:25 -0700

    Add startNewGame button to Loss

--- src/components/Loss.js
+++ src/components/Loss.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import './Loss.css';
+import { startNewGame } from '../actions';
 
-export const Loss = () => (
+export const Loss = ({ dispatch }) => (
   <div className="loss">
     <header>You Lose</header>
+    <button onClick={() => dispatch(startNewGame())}>Restart</button>
   </div>
 );
 
-export default Loss;
+Loss.defaultProps = { dispatch: () => {} };
+Loss.propTypes = { dispatch: PropTypes.func.isRequired };
+
+export default connect()(Loss);
